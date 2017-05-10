8e201b46179944fbeacb75cc004d43ae1f4f4e33
alexg622 <alex.gm62288@gmail.com>
2017-05-09 21:16:48 -0700

    Center Dog show page, Add star for pointing out comments, add trash icon for deleting comment. Start centering owner's show page. Change error message colors to red.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 	post 'dogs/:id/comments' => 'comments#create', as: :dog_comments
 	post 'dogs/:id/dislikes' => 'dislikes#create', as: :dog_dislikes
 	post 'dogs/:id/likes' => 'likes#create', as: :dog_likes
+	get 'dogs/:id/likes' => 'dogs#show'
+	get 'dogs/:id/dislikes' => 'dogs#show'
 	get '/login' => 'sessions#new' 
 	post '/login' => 'sessions#create'
 	delete '/logout' => 'sessions#destroy'
